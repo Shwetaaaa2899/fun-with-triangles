cdcdcd7cececc24829ddf6afc54af3e952165614
Shwetaaaa2899 <65586419+Shwetaaaa2899@users.noreply.github.com>
2022-09-06 00:18:31 +0530

    updated

--- area.js
+++ area.js
@@ -6,8 +6,9 @@ function fetchAns(a,b){
     a = Number(a);
     b = Number(b);
     const ans = a * b;
-    const finalAns  = 1/2*(ans) ;
-    console.log(ans);
+    const finalAns  = 0.5 * ans ;
+    // console.log(ans);
+    return finalAns;
 }
 function calculateArea(){
     if(inputList[0].value.length == 0 || inputList[1].value.length == 0){
@@ -15,7 +16,7 @@ function calculateArea(){
       }
       else{
     const ans = fetchAns(inputList[0].value,inputList[1].value);
-    outPut.innerText = "Area of a triangle  is " +ans +"units";
+    outPut.innerText = "Area of a triangle  is " +ans +" units";
       }
 };
 calcAreaButton.addEventListener('click',calculateArea)
